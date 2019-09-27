020bfebfa780980680ccae5a7af7f858ea93da07
Luis Dev <lcr@mediaimpact.pe>
2019-09-27 02:46:30 -0500

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,160 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+
+
+      <div class="row">
+
+        
+           <div class="col-6 card" style="width: 18rem;" v-for="board in boards" :key="board.id">
+              <div class="card-body">
+                <h5 class="card-title" v-text="board.name"></h5>
+                <button class="btn btn-primary"   @click="getListFromBoard(board.id)"> ver list</button>
+                
+                <template v-if="listFromBoard.board_id == board.id">
+                    
+                    <li class="card-text mt-3"  v-for="list in listFromBoard.lists"  :key="list.id"  > {{list.name}} 
+                      
+                      <button  class="btn btn-success"  @click="getCardsFromList(list.id)"> Ver cards </button>
+
+                        <template v-if="cardsFromList.list_id == list.id">
+                    
+                           <ul class="mt-3">
+                              <li class="card-text mt-2"   v-for="card in cardsFromList.cards"  :key="card.id"  > {{card.name}} 
+                              
+                                <a :href="card.shortUrl" target="_blank" >  ver en trello</a>
+                            </li>
+                           </ul>
+                        </template> 
+
+                    </li>
+                </template>
+
+             
+              
+              </div>
+         </div>
+
+      
+
+
+
+    </div>
+ 
+  </div>
+
+
+
+
+
+
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'app',
+  data () {
+
+    return {
+
+      token: '0ec0194b227b52871b7fb9004877a5a6dbd78d9d00e7c3f9d604c9c742168831',
+      key: '5f39fd32ce375d936870391efedad435',
+      boards: {},
+      listFromBoard:{},
+      cardsFromList: [],
+      cardsFilter:{},
+      parentUri: 'https://api.trello.com/1/members/me/'
+
+    }
+
+  },
   components: {
-    HelloWorld
+    
+  }, 
+
+  computed: {
+
+
+
+  },
+
+  methods: {
+
+    getBoards() {
+
+      let uri = `${this.parentUri}boards?key=${this.key}&token=${this.token}`
+
+      axios.get( uri )
+      .then(res => {
+
+        this.boards = res.data
+       
+      
+      })
+
+
+    }, 
+
+    getCards() {
+
+
+    }, 
+
+    getCardsFromList( list_id ) {
+
+
+      let uri = `https://api.trello.com/1/lists/${list_id}/cards?token=${this.token}&key=${this.key}`
+      
+      let me = this
+      
+      axios.get(uri)
+      .then (res =>  {
+        
+          me.cardsFromList = {
+            list_id,
+            cards: res.data
+          }
+
+         
+      })
+
+    
+    
+
+    },
+
+    getListFromBoard(board_id) {
+
+      let me = this
+      me.cardsFromList = {}
+      let uri = `https://api.trello.com/1/boards/${board_id}/lists?cards=none&card_fields=all&filter=open&fields=all&key=${this.key}&token=${this.token}`
+
+
+      axios.get(uri)
+        .then( res => {
+
+            me.listFromBoard = {
+              board_id,
+              lists: res.data
+            }
+
+
+        })
+    }
+
+    
+
+
+
+  },
+  mounted(){
+
+    this.getBoards()
+
+
   }
 }
 </script>
